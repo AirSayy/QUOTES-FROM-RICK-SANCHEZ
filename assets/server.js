81e11bbb5f9bcde4c6e82210cdea9f730d87471b
Eseoghene Eguakhide <eruotorese@yahoo.com>
2022-11-30 11:53:05 +0100

    update on project setup

--- assets/server.js
+++ assets/server.js
@@ -1,10 +1,21 @@
-
 const express = require('express'); 
+const bodyParser = require('body-parser')
 const app = express();
- app.get('/', (req, res) => {
-res.sendFile(__dirname + '/index.html')
-  
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html') 
 })
+
+
+app.post('/quotes', (req,res) => {
+  console.log(req.body)
+})
+
+
 app.listen(3000, function() {
   console.log('listening on 3000')
 })
