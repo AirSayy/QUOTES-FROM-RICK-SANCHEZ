14c4a12aa8641d3aef44d7b824b8fae97ad9b80e
Eseoghene Eguakhide <eruotorese@yahoo.com>
2022-11-30 16:37:59 +0100

    working on how to connect mongodb

--- assets/server.js
+++ assets/server.js
@@ -5,7 +5,14 @@ const MongoClient = require('mongodb').MongoClient
 
 app.use(bodyParser.urlencoded({extended:true}))
 
-
+MongoClient.connect('mongodb+srv://airsayy:Wendyy1990@cluster0.23ktvb1.mongodb.net/test?retryWrites=true&w=majority' , {
+  useUnifiedTopology : true
+},
+(err , client) => {
+if (err) return console.error(err)
+console.log('connected to database')
+// const db = client.db('Rick-Shanchez-quotes')
+})
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html') 
@@ -15,6 +22,8 @@ app.get('/', (req, res) => {
 app.post('/quotes', (req,res) => {
   console.log(req.body)
 })
+
+
 
 
 app.listen(3000, function() {
