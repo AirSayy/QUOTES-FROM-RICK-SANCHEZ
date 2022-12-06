6112fd5133d03df257a0d9e31e2cbe7fb1d98d46
Eseoghene Eguakhide <eruotorese@yahoo.com>
2022-12-06 18:46:52 +0100

    mongodb connection update

--- assets/server.js
+++ assets/server.js
@@ -1,31 +1,86 @@
 const express = require('express'); 
 const bodyParser = require('body-parser')
 const app = express();
-const MongoClient = require('mongodb').MongoClient
-
-app.use(bodyParser.urlencoded({extended:true}))
-
-MongoClient.connect('mongodb+srv://airsayy:Wendyy1990@cluster0.23ktvb1.mongodb.net/test?retryWrites=true&w=majority' , {
-  useUnifiedTopology : true
-},
-(err , client) => {
-if (err) return console.error(err)
-console.log('connected to database')
-// const db = client.db('Rick-Shanchez-quotes')
-})
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html') 
-})
-
-
-app.post('/quotes', (req,res) => {
-  console.log(req.body)
-})
 
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://username:password@cluster0.23ktvb1.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  app.use(bodyParser.urlencoded({extended:true}))
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
-})
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html') 
+  })
+    
+  app.post('/quotes', (req,res) => {
+    quotesCollection.insertOne(req.body)
+    .then(result => {
+      console.log(result)
+    // console.log(req.body)
+  })
+  .catch(error => console.error(error))
+  })
+    
+  app.listen(3000, function() {
+    console.log('listening on 3000')
+  })
+    
+
+  client.close();
+});
+
+
+
+
+
+// const MongoClient = require('mongodb').MongoClient
+
+
+
+// MongoClient.connect('mongodb+srv://username:password@cluster0.23ktvb1.mongodb.net/?retryWrites=true&w=majority', 
+// { useUnifiedTopology: true, 
+//   useNewUrlParser: true,
+// })
+// .then(client => {
+//   console.log('Connected to Database')
+//   const db = client.db('Rick-Shanchez-quotes')
+//   const quotesCollection = db.collection('quotes')
+
+//   app.use(bodyParser.urlencoded({extended:true}))
+
+//   app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html') 
+//   })
+    
+//   app.post('/quotes', (req,res) => {
+//     quotesCollection.insertOne(req.body)
+//     .then(result => {
+//       console.log(result)
+//     // console.log(req.body)
+//   })
+//   .catch(error => console.error(error))
+//   })
+    
+//   app.listen(3000, function() {
+//     console.log('listening on 3000')
+//   })
+    
+
+// })
+// .catch(error => console.error(error))
+
+// MongoClient.connect('mongodb+srv://username:password@cluster0.23ktvb1.mongodb.net/?retryWrites=true&w=majority' , {
+//   useUnifiedTopology : true,
+//   useNewUrlParser: true,
+//   serverApi: ServerApiVersion.v1
+// },
+// (err , client) => {
+// if (err) return console.error(err)
+// console.log('connected to database')
+// // const db = client.db('Rick-Shanchez-quotes')
+// })
+
