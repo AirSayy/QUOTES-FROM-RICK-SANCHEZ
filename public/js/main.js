ff4d8f2b2dcb5993372a10cac914d513f19dd372
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-02-09 17:57:20 +0100

    update

--- public/js/main.js
+++ public/js/main.js
@@ -11,7 +11,7 @@ Array.from(thumbText).forEach((element)=>{
 
 async function deleteQuote(){
     const sName = this.parentNode.childNodes[1].innerText
-    const bName = this.parentNode.childNodes[3].innerText
+    const bName = this.parentNode.childNodes[5].innerText
     try{
         const response = await fetch('deleteQuote', {
             method: 'delete',
@@ -32,8 +32,8 @@ async function deleteQuote(){
 
 async function addLike(){
     const cName = this.parentNode.childNodes[1].innerText
-    const qStr = this.parentNode.childNodes[3].innerText
-    const tLikes = Number(this.parentNode.childNodes[5].innerText)
+    const qStr = this.parentNode.childNodes[5].innerText
+    const tLikes = Number(this.parentNode.childNodes[7].innerText)
     try{
         const response = await fetch('addOneLike', {
             method: 'put',
